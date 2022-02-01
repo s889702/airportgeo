2233594e848a852709587310bfae667b82d81b6e
Chris <wang3947@purdue.edu>
2022-02-01 12:31:16 -0500

    final testing completed.

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ import time
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     rules: list = [
-        {"value": "place:\"Purdue University Airport (LAF)\" has:geo", "tag": "PUA"}
+        {"value": "place:\"Purdue University Airport\" has:geo", "tag": "PUA"},
+        {"value": "place:\"Boston Logan International Airport\" has:geo", "tag": "BOS"},
+        {"value": "place:\"Dallas Love Field\" has:geo", "tag": "DAL"},
+        {"value": "place:\"Ontario International Airport\" has:geo", "tag": "ONT"},
+        {"value": "place:\"Akron-Canton Airport\" has:geo", "tag": "CAK"},
+        {"value": "place:\"Jacksonville Albert J Ellis Airport\" has:geo", "tag": "OAJ"}
     ]
 
     # deletelist = {
     #         "delete": {
-    #             "ids": ['1475932361049190408'] # example id
+    #             "ids": ['1487972196735717378'] # example id
     #         }
     #     }
     class Stream(FilteredStream):
@@ -22,7 +27,7 @@ if __name__ == '__main__':
 
     stream = Stream()
     stream.add_rule(data={"add": rules})
-    # stream.delete_rule(data=deletelist)
+    #stream.delete_rule(data=deletelist)
     print(stream.get_rules())
 
 while True:
