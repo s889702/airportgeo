00b229c41f720bc59e53443d890c5bbcd66545f0
Chris <wang3947@purdue.edu>
2022-02-01 02:01:11 -0500

    final testing completed.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from twitter_stream import FilteredStream
+import json
 import time
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -28,12 +29,14 @@ while True:
 
     try:
         for tweet in stream.connect():
-            with open('output.txt', 'a') as f:
-                print(json.dumps(tweet), file=f)
             print(tweet)
+            with open('test.txt', 'a') as f:
+                print(json.dumps(tweet), file=f)
+
 
     except:
         print("no tweet collected")
         time.sleep(1)
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
